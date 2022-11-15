650e3b40a92d903a044c5f6b5bf6f5f1d74f8244
Inge-egnI <79852368+Inge-egnI@users.noreply.github.com>
2022-11-15 12:40:22 +0800

    change page description

--- site.config.ts
+++ site.config.ts
@@ -15,7 +15,7 @@ export default siteConfig({
   author: 'SuZY',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'PPESA Humanities Association one-year anniversary Site',
 
   // social usernames (optional)
   // twitter: 'transitive_bs',
